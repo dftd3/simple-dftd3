36c3671aa16fc5c4deec8dd0fefe56a0dd4bcad0
Sebastian Ehlert <sehlert@microsoft.com>
2024-12-01 16:48:12 +0100

    Cleanup Python API for dftd3.library

--- python/dftd3/library.py
+++ python/dftd3/library.py
@@ -20,6 +20,9 @@ for memory handling.
 """
 
 import functools
+from typing import Optional
+
+import numpy as np
 
 try:
     from ._libdftd3 import ffi, lib
@@ -41,6 +44,59 @@ def get_api_version() -> str:
         api_version % 10000 // 100,
         api_version % 100,
     )
+
+
+class Handle:
+    def __init__(self, handle):
+        self.handle = handle
+
+    @classmethod
+    def with_gc(cls, handle):
+        return cls(ffi.gc(handle, cls._delete))
+
+    @classmethod
+    def null(cls):
+        return cls(ffi.NULL)
+
+    @staticmethod
+    def _delete(handle):
+        raise NotImplementedError("Delete function not implemented")
+
+
+class StructureHandle(Handle):
+    @staticmethod
+    def _delete(handle):
+        """Delete a DFT-D3 molecular structure data object"""
+        ptr = ffi.new("dftd3_structure *")
+        ptr[0] = handle
+        lib.dftd3_delete_structure(ptr)
+
+
+class ModelHandle(Handle):
+    @staticmethod
+    def _delete(handle):
+        """Delete a DFT-D3 dispersion model object"""
+        ptr = ffi.new("dftd3_model *")
+        ptr[0] = handle
+        lib.dftd3_delete_model(ptr)
+
+
+class ParamHandle(Handle):
+    @staticmethod
+    def _delete(handle):
+        """Delete a DFT-D3 damping parameteter object"""
+        ptr = ffi.new("dftd3_param *")
+        ptr[0] = handle
+        lib.dftd3_delete_param(ptr)
+
+
+class GCPHandle(Handle):
+    @staticmethod
+    def _delete(handle):
+        """Delete a counter-poise parameter object"""
+        ptr = ffi.new("dftd3_gcp *")
+        ptr[0] = handle
+        lib.dftd3_delete_gcp(ptr)
 
 
 def _delete_error(mol):
@@ -72,148 +128,191 @@ def error_check(func):
     return handle_error
 
 
-def _delete_structure(mol):
-    """Delete a DFT-D3 molecular structure data object"""
-    ptr = ffi.new("dftd3_structure *")
-    ptr[0] = mol
-    lib.dftd3_delete_structure(ptr)
-
-
-def new_structure(natoms, numbers, positions, lattice, periodic):
+def new_structure(
+    natoms: int,
+    numbers: np.ndarray,
+    positions: np.ndarray,
+    lattice: Optional[np.ndarray],
+    periodic: Optional[np.ndarray],
+) -> StructureHandle:
     """Create new molecular structure data"""
-    return ffi.gc(
+    return StructureHandle.with_gc(
         error_check(lib.dftd3_new_structure)(
             natoms,
-            numbers,
-            positions,
-            lattice,
-            periodic,
-        ),
-        _delete_structure,
+            _cast("int*", numbers),
+            _cast("double*", positions),
+            _cast("double*", lattice),
+            _cast("bool*", periodic),
+        )
     )
 
 
-def _delete_model(disp):
-    """Delete a DFT-D3 dispersion model object"""
-    ptr = ffi.new("dftd3_model *")
-    ptr[0] = disp
-    lib.dftd3_delete_model(ptr)
-
-
-def new_d3_model(mol):
+def new_d3_model(mol: StructureHandle) -> ModelHandle:
     """Create new D3 dispersion model"""
-    return ffi.gc(error_check(lib.dftd3_new_d3_model)(mol), _delete_model)
+    return ModelHandle.with_gc(error_check(lib.dftd3_new_d3_model)(mol.handle))
 
 
-set_model_realspace_cutoff = error_check(lib.dftd3_set_model_realspace_cutoff)
+def set_model_realspace_cutoff(
+    disp: ModelHandle, disp2: float, disp3: float, cn: float
+) -> None:
+    """Set the realspace cutoff for the dispersion model"""
+    return error_check(lib.dftd3_set_model_realspace_cutoff)(
+        disp.handle, disp2, disp3, cn
+    )
 
 
-def _delete_param(param):
-    """Delete a DFT-D3 damping parameteter object"""
-    ptr = ffi.new("dftd3_param *")
-    ptr[0] = param
-    lib.dftd3_delete_param(ptr)
-
-
-def new_zero_damping(s6, s8, s9, rs6, rs8, alp):
+def new_zero_damping(
+    s6: float, s8: float, s9: float, rs6: float, rs8: float, alp: float
+) -> ParamHandle:
     """Create new zero damping parameters"""
-    return ffi.gc(
-        error_check(lib.dftd3_new_zero_damping)(s6, s8, s9, rs6, rs8, alp),
-        _delete_param,
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_new_zero_damping)(s6, s8, s9, rs6, rs8, alp)
     )
 
 
-def load_zero_damping(method, atm):
+def load_zero_damping(method: str, atm: bool) -> ParamHandle:
     """Load zero damping parameters from internal storage"""
-    return ffi.gc(error_check(lib.dftd3_load_zero_damping)(method, atm), _delete_param)
+    return ParamHandle.with_gc(error_check(lib.dftd3_load_zero_damping)(_char(method), atm))
 
 
-def new_rational_damping(s6, s8, s9, a1, a2, alp):
+def new_rational_damping(
+    s6: float, s8: float, s9: float, a1: float, a2: float, alp: float
+) -> ParamHandle:
     """Create new rational damping parameters"""
-    return ffi.gc(
-        error_check(lib.dftd3_new_rational_damping)(s6, s8, s9, a1, a2, alp),
-        _delete_param,
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_new_rational_damping)(s6, s8, s9, a1, a2, alp)
     )
 
 
-def load_rational_damping(method, atm):
+def load_rational_damping(method: str, atm: bool) -> ParamHandle:
     """Load rational damping parameters from internal storage"""
-    return ffi.gc(
-        error_check(lib.dftd3_load_rational_damping)(method, atm), _delete_param
-    )
+    return ParamHandle.with_gc(error_check(lib.dftd3_load_rational_damping)(_char(method), atm))
 
 
-def new_mzero_damping(s6, s8, s9, rs6, rs8, alp, bet):
+def new_mzero_damping(
+    s6: float, s8: float, s9: float, rs6: float, rs8: float, alp: float, bet: float
+) -> ParamHandle:
     """Create new modified zero damping parameters"""
-    return ffi.gc(
-        error_check(lib.dftd3_new_mzero_damping)(s6, s8, s9, rs6, rs8, alp, bet),
-        _delete_param,
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_new_mzero_damping)(s6, s8, s9, rs6, rs8, alp, bet)
     )
 
 
-def load_mzero_damping(method, atm):
+def load_mzero_damping(method: str, atm: bool) -> ParamHandle:
     """Load modified zero damping parameters from internal storage"""
-    return ffi.gc(error_check(lib.dftd3_load_mzero_damping)(method, atm), _delete_param)
+    return ParamHandle.with_gc(error_check(lib.dftd3_load_mzero_damping)(_char(method), atm))
 
 
-def new_mrational_damping(s6, s8, s9, a1, a2, alp):
+def new_mrational_damping(
+    s6: float, s8: float, s9: float, a1: float, a2: float, alp: float
+) -> ParamHandle:
     """Create new modified rational damping parameters"""
-    return ffi.gc(
-        error_check(lib.dftd3_new_mrational_damping)(s6, s8, s9, a1, a2, alp),
-        _delete_param,
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_new_mrational_damping)(s6, s8, s9, a1, a2, alp)
     )
 
 
-def load_mrational_damping(method, atm):
+def load_mrational_damping(method: str, atm: bool) -> ParamHandle:
     """Load modified rational damping parameters from internal storage"""
-    return ffi.gc(
-        error_check(lib.dftd3_load_mrational_damping)(method, atm), _delete_param
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_load_mrational_damping)(_char(method), atm)
     )
 
 
-def new_optimizedpower_damping(s6, s8, s9, a1, a2, alp, bet):
+def new_optimizedpower_damping(
+    s6: float, s8: float, s9: float, a1: float, a2: float, alp: float, bet: float
+) -> ParamHandle:
     """Create new optimized power damping parameters"""
-    return ffi.gc(
-        error_check(lib.dftd3_new_optimizedpower_damping)(s6, s8, s9, a1, a2, alp, bet),
-        _delete_param,
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_new_optimizedpower_damping)(s6, s8, s9, a1, a2, alp, bet)
     )
 
 
-def load_optimizedpower_damping(method, atm):
+def load_optimizedpower_damping(method: str, atm: bool) -> ParamHandle:
     """Load optimized power damping parameters from internal storage"""
-    return ffi.gc(
-        error_check(lib.dftd3_load_optimizedpower_damping)(method, atm), _delete_param
+    return ParamHandle.with_gc(
+        error_check(lib.dftd3_load_optimizedpower_damping)(_char(method), atm)
     )
 
 
-update_structure = error_check(lib.dftd3_update_structure)
-get_dispersion = error_check(lib.dftd3_get_dispersion)
-get_pairwise_dispersion = error_check(lib.dftd3_get_pairwise_dispersion)
+def update_structure(
+    mol: StructureHandle, positions: np.ndarray, lattice: Optional[np.ndarray]
+) -> None:
+    """Update the molecular structure data"""
+    return error_check(lib.dftd3_update_structure)(
+        mol.handle,
+        _cast("double*", positions),
+        _cast("double*", lattice),
+    )
 
 
-def _delete_gcp(gcp):
-    """Delete a geometric counter-poise object"""
-    ptr = ffi.new("dftd3_gcp *")
-    ptr[0] = gcp
-    lib.dftd3_delete_gcp(ptr)
+def get_dispersion(
+    mol: StructureHandle,
+    disp: ModelHandle,
+    param: ParamHandle,
+    energy: np.ndarray,
+    gradient: Optional[np.ndarray],
+    sigma: Optional[np.ndarray],
+) -> None:
+    return error_check(lib.dftd3_get_dispersion)(
+        mol.handle,
+        disp.handle,
+        param.handle,
+        _cast("double*", energy),
+        _cast("double*", gradient),
+        _cast("double*", sigma),
+    )
 
 
-def load_gcp_param(mol, method: str, basis: str):
+def get_pairwise_dispersion(
+    mol: StructureHandle,
+    disp: ModelHandle,
+    param: ParamHandle,
+    energy2: np.ndarray,
+    energy3: np.ndarray,
+) -> None:
+    return error_check(lib.dftd3_get_pairwise_dispersion)(
+        mol.handle,
+        disp.handle,
+        param.handle,
+        _cast("double*", energy2),
+        _cast("double*", energy3),
+    )
+
+
+def load_gcp_param(mol, method: Optional[str], basis: Optional[str]) -> GCPHandle:
     """Load GCP parameters from internal storage"""
-    return ffi.gc(
-        error_check(lib.dftd3_load_gcp_param)(mol, method.encode(), basis.encode()),
-        _delete_gcp,
+    return GCPHandle.with_gc(
+        error_check(lib.dftd3_load_gcp_param)(mol, _char(method), _char(basis))
     )
 
 
-set_gcp_realspace_cutoff = error_check(lib.dftd3_set_gcp_realspace_cutoff)
-get_counterpoise = error_check(lib.dftd3_get_counterpoise)
+def set_gcp_realspace_cutoff(gcp: GCPHandle, bas: float, srb: float) -> None:
+    error_check(lib.dftd3_set_gcp_realspace_cutoff)(gcp.handle, bas, srb)
 
 
-def _ref(ctype, value):
-    """Create a reference to a value"""
-    if value is None:
-        return ffi.NULL
-    ref = ffi.new(ctype + "*")
-    ref[0] = value
-    return ref
+def get_counterpoise(
+    mol: StructureHandle,
+    gcp: GCPHandle,
+    energy: np.ndarray,
+    gradient: Optional[np.ndarray],
+    sigma: Optional[np.ndarray],
+) -> None:
+    """Get the counterpoise energy"""
+    return error_check(lib.dftd3_get_counterpoise_energy)(
+        mol.handle,
+        gcp.handle,
+        _cast("double*", energy),
+        _cast("double*", gradient),
+        _cast("double*", sigma),
+    )
+
+
+def _char(value: Optional[str]):
+    """Convert a string to a C char array"""
+    return ffi.new("char[]", value.encode()) if value is not None else ffi.NULL
+
+
+def _cast(ctype: str, array: Optional[np.ndarray]):
+    """Cast a numpy array to a FFI pointer"""
+    return ffi.cast(ctype, array.ctypes.data) if array is not None else ffi.NULL
